079873964e37a7db5cd70c6229c8d296dde82d4f
勝崴 施 <4b1g0178@office.stust.edu.tw>
2023-04-19 09:47:35 +0800

    加入專案檔案。

---
+++ ITSSA_24/ITSSA_24.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>  
+using namespace std;
+
+int main() {
+
+    double r = 0.0, ans, x, sum = 0.0;  //x=利息    
+    int n = 0, p = 0, i = 0;
+
+    cin >> r >> n >> p;
+
+    while (i < n) {
+
+        x = (p + sum) * r;
+        sum = sum + x + p;
+        i++;
+
+    }
+
+    cout << (long long)sum << endl;
+    return 0;
+}
